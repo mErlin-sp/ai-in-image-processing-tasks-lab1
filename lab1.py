b5bf3cde93fe9803826c2c7d463cb05f64504f4b
Oleksandr <merlin.ua.1@gmail.com>
2023-05-21 22:56:59 +0300

    Update lab1

--- lab1.py
+++ lab1.py
@@ -2,84 +2,86 @@ import imutils as imutils
 import numpy as np
 import cv2
 
-print(cv2.__version__)
+print('Lab1. OpenCV version: ' + cv2.__version__)
 
 # завантажуємо файл як кольорове зображення і як сіре зображення
-img = cv2.imread('daenerys-targaryen.jpg')
-img_gray = cv2.imread('daenerys-targaryen.jpg', 0)
+img = cv2.imread('daenerys.jpg')
+img_gray = cv2.imread('daenerys.jpg', 0)
 
-cv2.imshow('Raw image', img)
+cv2.imshow(' Raw image ', img)
 # закриваються всі вікна після натискання довільної клавіші
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # збереження зображення в файл
-cv2.imwrite('daenerys-targaryen-1.jpg', img)
+cv2.imwrite('daenerys-1.jpg', img)
 
 # вивід розміру зображення
 (h, w, d) = img.shape  #
-print(" width ={}, height ={}, depth ={}".format(w, h, d))
+print(' width ={}, height ={}, depth ={}'.format(w, h, d))
 
 # доступ до RGB пікселя з координатами x=50 , y=100
 (B, G, R) = img[100, 50]
-print("R={}, G={}, B={}".format(R, G, B))
+print('R={}, G={}, B={}'.format(R, G, B))
 
 # extract a square ROI ( Region of Interest ) from the
-# input image starting at x=1500 ,y=470 at ending at x=1100 ,y=6
-roi = img[6:470, 1100:1500]
-cv2.imshow("ROI", roi)
+# input image starting at x=350 ,y=250 at ending at x=650 ,y=0
+roi = img[0:250, 350:650]
+cv2.imshow(' ROI ', roi)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# resize the image to 960x540px , ignoring aspect ratio
-img = cv2.resize(img, (960, 540))
-cv2.imshow("Fixed Resizing", img)
+# resize the image to 800x450px , ignoring aspect ratio
+img = cv2.resize(img, (800, 450))
+cv2.imshow(' Fixed Resizing ', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# resize the image to 960x540px, saving aspect ratio
+# resize the image to 800x450px, saving aspect ratio
 h, w = img.shape[0:2]  # висота і ширина оригінального зображення
-h_new = 540  # висота нового зображення
+h_new = 450  # висота нового зображення
 ratio = w / h  # відношення ширини до висоти
 w_new = int(h_new * ratio)  # ширина нового зображення
 resized = cv2.resize(img, (w_new, h_new))  # зміна розміру оригінального зображення
 print(resized.shape)
-cv2.imshow('Fixed Resizing', resized)
+cv2.imshow(' Fixed Resizing ', resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # поворот зображення на 45 градусів
 rotated = imutils.rotate(img, -45)
-cv2.imshow(" Imutils Rotation ", rotated)
+cv2.imshow(' Imutils Rotation ', rotated)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # apply a Gaussian blur with a 11x11 kernel to the image to smooth it ,
 # useful when reducing high frequency noise
 blurred = cv2.GaussianBlur(img, (11, 11), 0)
-cv2.imshow(" Blurred ", blurred)
+cv2.imshow(' Blurred ', blurred)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # draw rectangle
-cv2.rectangle(img, (550, 3), (750, 235), (0, 0, 255), 2)
-cv2.imshow(" Rectangle ", img)
+cv2.rectangle(img, (350, 0), (650, 250), (0, 0, 255), 2)
+cv2.imshow(' Rectangle ', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # draw line
-zero_img = np.zeros((1000, 1000, 3), np.uint8)
-points = np.array([[600, 200], [910, 641], [300, 300], [0, 0]])
-cv2.polylines(zero_img, np.int32([points]), 1, (255, 255, 255))
-cv2.imshow('1', zero_img)
+points = np.array(
+    [[262, 448], [273, 386], [306, 352], [307, 318], [345, 294], [379, 269], [386, 237], [381, 216], [394, 195],
+     [387, 161], [386, 124], [392, 82], [402, 46], [417, 13], [435, -1], [600, 0], [604, 14], [613, 33], [622, 45],
+     [626, 77], [634, 103], [630, 118], [626, 137], [630, 173], [628, 199], [631, 211], [644, 233], [645, 246],
+     [662, 263], [686, 269], [718, 281], [732, 292], [734, 306], [729, 319], [732, 334], [747, 369], [753, 400],
+     [758, 428], [758, 449]])
+cv2.polylines(img, np.int32([points]), 2, (255, 0, 0))
+cv2.imshow(' Lines ', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # draw circle
-zero_img = np.zeros((200, 200, 3), np.uint8)
-output = img.copy()
-cv2.circle(zero_img, (100, 100), 50, (0, 0, 255), 2)
-cv2.imshow(" Circle ", zero_img)
+cv2.circle(img, (494, 114), 128, (0, 255, 0), 2)
+cv2.imshow(' Circle ', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -87,7 +89,7 @@ cv2.destroyAllWindows()
 font = cv2.FONT_HERSHEY_SIMPLEX
 font1 = cv2.FONT_HERSHEY_COMPLEX
 font2 = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
-cv2.putText(img, 'Daenerys Targaryen', (550, 300), font, 1, (0, 0, 255), 2, cv2.LINE_4)
-cv2.imshow(" Text ", img)
+cv2.putText(img, 'Daenerys', (400, 300), font, 1, (102, 0, 204), 2, cv2.LINE_4)
+cv2.imshow(' Text ', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
